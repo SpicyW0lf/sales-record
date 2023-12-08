833b759be4fe85888dd7be4757db399d2fae54a4
SpicyW0lf <popafo68@gmail.com>
2023-12-08 17:52:00 +0300

    Финальный коммит

--- src/main/java/com/example/salesrecord/configs/SecurityConfig.java
+++ src/main/java/com/example/salesrecord/configs/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.example.salesrecord.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +21,10 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
+                        .requestMatchers("/user").hasRole("ADMIN")
+                        .requestMatchers("/user/*").hasRole("ADMIN")
+                        .requestMatchers("/purchases").hasAnyRole("ADMIN", "MANAGER", "CASHIER")
+                        .requestMatchers("/purchase/*").hasAnyRole("ADMIN", "MANAGER", "CASHIER")
                         .requestMatchers("/category").hasRole("MANAGER")
                         .requestMatchers("/category/*").hasRole("MANAGER")
                         .anyRequest().authenticated()
